9594b6d58e5936767747520b1a5bbee1ba277a30
Hugo Josefson <hugo@josefson.org>
2024-10-06 01:05:55 +0200

    feat: make sort-unique generic

--- string/sort-unique.ts
+++ string/sort-unique.ts
@@ -3,6 +3,6 @@
  * @param strings The strings to sort and remove duplicates from.
  * @returns The sorted strings, with duplicates removed.
  */
-export function sortUnique(strings: string[]): string[] {
-  return [...(new Set(strings)).values()].sort();
+export function sortUnique<T extends string>(strings: T[]): T[] {
+  return [...(new Set(strings)).values()].sort() as T[];
 }
